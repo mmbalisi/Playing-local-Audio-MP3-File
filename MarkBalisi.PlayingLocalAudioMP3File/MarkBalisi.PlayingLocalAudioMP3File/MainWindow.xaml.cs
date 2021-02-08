07f4e3315336a20126345fff31551c0fdbdd6b22
DESKTOP-0UPI14J\hp <mmbalisi@csmbataan.ph.education>
2021-02-08 18:07:58 +0800

    The MediaPlayer Class- Added a working button named Open Audio File that plays music in your file manager.

--- MarkBalisi.PlayingLocalAudioMP3File/MarkBalisi.PlayingLocalAudioMP3File/MainWindow.xaml.cs
+++ MarkBalisi.PlayingLocalAudioMP3File/MarkBalisi.PlayingLocalAudioMP3File/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using Microsoft.Win32;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,20 @@ namespace MarkBalisi.PlayingLocalAudioMP3File
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void btnOpenAudioFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
+                mediaPlayer.Play();
+            }
         }
     }
 }
